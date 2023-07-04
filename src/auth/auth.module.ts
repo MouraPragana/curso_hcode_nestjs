0a826622acf36cdb6fa8b9aef848b0ea103e6448
Mateus Moura <mateuspragana@gmail.com>
2023-07-04 16:09:31 -0300

    feat: finalizando aula - Variáveis de ambiente

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,16 +1,16 @@
 import { Module, forwardRef } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
-import { AuthService } from './auth.service';
-import { AuthController } from './auth.controller';
-import { UserModule } from 'src/user/user.module';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { UserModule } from 'src/user/user.module';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
 
 @Module({
   imports: [
     PrismaModule,
     forwardRef(() => UserModule),
     JwtModule.register({
-      secret: 'segredoTeste',
+      secret: process.env.JWT_SECRET,
     }),
   ],
   controllers: [AuthController],
