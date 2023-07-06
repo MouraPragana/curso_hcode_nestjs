a852a681fa4edd508d3ca838ae5f7c87d151a570
Mateus Moura <mateuspragana@gmail.com>
2023-07-06 13:22:42 -0300

    feat: finalizando aula - mailer module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,16 +6,31 @@ import { AuthModule } from './auth/auth.module';
 import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 import { APP_GUARD } from '@nestjs/core';
 import { ConfigModule } from '@nestjs/config';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { PugAdapter } from '@nestjs-modules/mailer/dist/adapters/pug.adapter';
 
 @Module({
   imports: [
+    MailerModule.forRoot({
+      transport:
+        'smtps://clifton.cassin48@ethereal.email:bGHfyaYmVq7sJxrX8W@smtp.ethereal.email',
+      defaults: {
+        from: '"nest-modules" <modules@nestjs.com>',
+      },
+      template: {
+        dir: __dirname + '/templates',
+        adapter: new PugAdapter(),
+        options: {
+          strict: true,
+        },
+      },
+    }),
     ConfigModule.forRoot(),
     UserModule,
     AuthModule,
     ThrottlerModule.forRoot({
       ttl: 60,
       limit: 10,
-      ignoreUserAgents: [/googlebot/gi],
     }),
   ],
   controllers: [AppController],
